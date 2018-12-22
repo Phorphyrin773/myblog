a9355d235e3480777e57f112dbc19ddea4f71b36
EC2 Default User <ec2-user@ip-172-31-28-67.ap-southeast-1.compute.internal>
2018-12-22 09:21:21 +0000

    signin

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,6 +7,11 @@ class TweetsController < ApplicationController
   def new
   end
   
+  def create
+    Tweet.create(tweet_params)
+  end
+  
+    
   def show
   end
   
@@ -14,6 +19,12 @@ class TweetsController < ApplicationController
   end
   
   def destroy
+  end
+  
+  
+  private
+  def tweet_params
+    params.permit(:title, :text)
   end
 
 end
