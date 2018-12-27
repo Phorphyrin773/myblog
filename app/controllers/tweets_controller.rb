00ff9c2d823b20e77370ed5d3c1cf5225e938f45
EC2 Default User <ec2-user@ip-172-31-28-67.ap-southeast-1.compute.internal>
2018-12-27 09:22:21 +0000

    edit update

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,27 +5,34 @@ class TweetsController < ApplicationController
   end
   
   def new
+    @tweet = Tweet.new
   end
   
   def create
-    binding.pry
-    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    Tweet.create(image: tweet_params[:image], title: tweet_params[:title], text: tweet_params[:text], user_id: current_user.id)
   end
   
-    
-  def show
-  end
   
   def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
   end
   
   def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
   end
   
   
   private
   def tweet_params
-    params.permit(:title, :text, :image)
+    params.require(:tweet).permit(:title, :text, :image)
   end
 
 end
